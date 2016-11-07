cbe2a32caa07edd39981397af1887b24e47821ed
nguyenchihien <hiennc@onlikeservices.com>
2016-11-07 22:29:44 +0700

    1029

---
+++ assets/setting.js
@@ -0,0 +1,72 @@
+// ============= HOME ==============
+var Page = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Nguyễn Trần Chương ",
+    subHeading: "Website để thể hiện. <strong>Landing page</strong> để thu hút khách hàng",
+    mainHeading: "Tôi muốn đem <b>BHNT</b> đến với <b>mọi người</b>",
+    CTA: "Tôi muốn có ngay Landing Page",
+    brief: "Cơ hội nhận discount or Free Landing Page. Chỉ cần theo dõi các chương trình cộng đồng của chúng tôi",
+
+    // ============= VARS ==============
+    url : "http://nguyentranchuong.chinhlatoi.top/", //-- co slash cuoi /
+    mainkeyWord : "Nguyen Tran Chuong",
+    longkeyWords : ["Email Marketing", "Landing Page", "IT services"],
+    facebookAppId : "303370230021409", //-- your appID
+    googleAnalyticId : "UA-82197343-2", //-- your id
+    formId: "https://docs.google.com/forms/d/e/1FAIpQLScjBXL-M0eRB0qhX1xSV5sWWAwsXbNqtjAGm3aq0-l5EZAisg/viewform?embedded=true",
+}
+
+var Social = {
+    //-- de trong "" de giu nguyen default text
+    email: "maito:chuongnt@onlikeservices.com",
+    facebook: "https://www.facebook.com/chuong.nguyen.104418",
+    twitter: "twitter.com",
+    google: "google.com",
+    instagram: "instagram.com",
+    pinterest: "pinterest.com",
+    linkedin: "linkedin.com"
+}
+
+var colors = [
+    "red","blue","green","orange",  // 0 1 2 3  
+    "pink", "purple", "light-blue", "cyan", // 4 5 6 7  
+    "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
+    "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
+    "brown", "grey", "blue-grey"      // 16 17 18 19
+];
+
+colors = ['green']
+var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
+var chooseColor = colors[i].toString();
+
+
+// ============= PERSONAL ==============
+var Person = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Nguyễn Trần Chương",
+    fullname: "<strong>Nguyễn Trần Chương</strong>.",
+    slogan: "Dịch vụ IT tại nhà",
+    sex: '<i class="fa male"></i>', //-- male, female
+    phone: 'tel:0933444965',
+    // message: "Tôi đã sẵn sàng, còn bạn thì sao?", //-- thong diep chinh hien nay!
+    message: "Luôn sẵn sàng làm hài lòng các khách hàng",
+    
+    email: "mailto:chuongnt@onlikeservices.com",
+    facebook: 'https://www.facebook.com/chuong.nguyen.104418',
+    facebook: "https://www.facebook.com/chuong.nguyen.104418",
+    twitter: "twitter.com",
+    google: "google.com",
+    instagram: "instagram.com",
+    pinterest: "pinterest.com",
+    linkedin: "https://www.linkedin.com/",
+    CTA: "Tôi muốn có ngay Landing Page",
+}
+
+
+var Social = {
+
+}
+
+
+
+    
